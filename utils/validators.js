79c4463827d4f98683906fa4d8b6ed68df0687ea
unknown <kirildim16@gmail.com>
2020-08-29 12:00:33 +0300

    validators fixed

--- utils/validators.js
+++ utils/validators.js
@@ -1,9 +1,8 @@
 const {body, validationResult} = require('express-validator/check')
 
 module.exports = [
-    body('title', 'title should be is alphanumeric (length: min: 2, max: 20)')
-        .isLength({ min: 2, max: 20 })
-        .isAlphanumeric(),
-    body('descr', 'descr: (length: min: 2, max: 200)')
-        .isLength({ min: 2, max: 200 })
+    body('title', 'title: (length: min: 2, max: 20)')
+        .isLength({ min: 2, max: 20 }),
+    body('descr', 'descr: (length: min: 2, max: 300)')
+        .isLength({ min: 2, max: 300 })
 ]
